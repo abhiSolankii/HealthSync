3c49ea0ef83bb31dfc49b02ccd100c8168532701
Abhishek solanki <abhishek@Abhisheks-MacBook-Air.local>
2024-09-18 05:42:42 +0530

    set backend api of vercel

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL:
     import.meta.env.VITE_NODE_ENV_PROD === "true"
-      ? "https://health-sync-three.vercel.app/api"
+      ? "https://health-sync-six.vercel.app/api"
       : "http://localhost:4000/api",
   withCredentials: true,
 });
