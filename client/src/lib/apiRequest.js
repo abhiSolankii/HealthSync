ce581e34d65025cf6e6e56cf487cf81d87ea2717
Abhishek solanki <abhishek@Abhisheks-MacBook-Air.local>
2024-09-18 07:59:50 +0530

    deploy

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL:
-    import.meta.env.VITE_NODE_ENV_PROD === "true"
-      ? "https://health-sync-six.vercel.app/api"
-      : "http://localhost:4000/api",
+  //for deployment
+  baseURL: "https://health-sync-six.vercel.app/api",
+
+  //for development
+  // baseURL: "http://localhost:4000/api",
   withCredentials: true,
 });
 
