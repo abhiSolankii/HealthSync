3a8fd32ed97a646d65281fe416f79ef69726c71b
Abhishek solanki <abhishek@Abhisheks-MacBook-Air.local>
2024-09-18 09:03:35 +0530

    deploy

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   //for deployment
-  baseURL: "https://healthsync-bx9o.onrender.com",
+  baseURL: "https://healthsync-bx9o.onrender.com/api",
 
   //for development
   // baseURL: "http://localhost:4000/api",
